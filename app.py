83f00f6b62321510fbb4c9f0cbb80f4e9a28f1da
nkojic <nenad.kojic.konsalting@gmail.com>
2025-06-14 08:33:56 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+import numpy as np
+
+# Učitavanje modela
+model = tf.keras.models.load_model("model_ann.h5")
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['inputs']
+    input_array = np.array([data])
+    prediction = model.predict(input_array)[0][0]
+    return jsonify({'prediction': float(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
