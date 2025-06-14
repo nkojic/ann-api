751164ce50482359c17c9eef70b6a3fe10e2956c
nkojic <nenad.kojic.konsalting@gmail.com>
2025-06-14 09:37:08 +0200

    Fix: use correct host and port for Render

--- app.py
+++ app.py
@@ -26,4 +26,6 @@ def predict():
     return jsonify({'prediction': float(prediction)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
